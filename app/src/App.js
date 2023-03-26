8a9b4c234fad043ea9a49fa5dbe5d2bfc8a5c247
adamali90 <riktechsolutions@gmail.com>
2023-03-26 23:04:06 +0300

    first commit

---
+++ app/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function App() {
+    return (
+      <div>
+        <div> <h1 className = "text-xs">   Hello world! with tailwind  </h1> </div> 
+   
+     
+    <div className = "bg-red-200 " > Hello world</div>
+    <h1> without tailwind</h1>
+    </div>
+    );
+  }
+  
+  export default App;
+  
